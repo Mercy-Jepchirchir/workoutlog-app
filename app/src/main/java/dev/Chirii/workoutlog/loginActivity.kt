a8a1b0845b10dd9407ee59e4c6cefe339aaf5831
Mercy-Jepchirchir <mjepchirchirkemboi@gmail.com>
2022-06-04 09:42:49 +0300

    added Login activity

--- app/src/main/java/dev/Chirii/workoutlog/loginActivity.kt
+++ app/src/main/java/dev/Chirii/workoutlog/loginActivity.kt
@@ -2,10 +2,43 @@ package dev.Chirii.workoutlog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class loginActivity : AppCompatActivity() {
+    lateinit var btnLogin: Button
+    lateinit var tilEmail: TextInputLayout
+    lateinit var tilPassword : TextInputLayout
+    lateinit var  etEmail: TextInputEditText
+    lateinit var etPassword:  TextInputEditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btnLogin = findViewById(R.id.btnLogin)
+        tilEmail = findViewById(R.id.tilEmail)
+        tilPassword = findViewById(R.id.tilPassword)
+        etEmail = findViewById(R.id.etEmail)
+        etPassword = findViewById(R.id.etPassword)
+
+        btnLogin.setOnClickListener{
+            validateLogin()
+        }
     }
+    fun validateLogin(){
+        var email = etEmail.text.toString()
+        var password = etPassword.text.toString()
+
+        if (email.isBlank()){
+            tilEmail.error = getString(R.string.email_required)
+        }
+        if (email.isBlank()){
+            tilPassword.error = getString(R.string.password_required)
+        }
+
+    }
+
 }
