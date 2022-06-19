90ea8a36c98da71bb06265e0247418bec6859858
Mercy-Jepchirchir <mjepchirchirkemboi@gmail.com>
2022-06-19 11:07:02 +0300

    Change singup and home to bindingview

--- app/src/main/java/dev/Chirii/workoutlog/HomeActivity.kt
+++ app/src/main/java/dev/Chirii/workoutlog/HomeActivity.kt
@@ -4,25 +4,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.FragmentContainerView
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import dev.Chirii.workoutlog.databinding.ActivityHomeBinding
+import dev.Chirii.workoutlog.databinding.ActivityLoginBinding
 
 class HomeActivity : AppCompatActivity() {
-    lateinit var fcvHome: FragmentContainerView
-    lateinit var bnvHome: BottomNavigationView
+  lateinit var binding:ActivityHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        castViews()
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         setupBottomNav()
+
     }
 
-    fun castViews() {
-        fcvHome = findViewById(R.id.fcvHome)
-        bnvHome = findViewById(R.id.bnvHome)
-    }
+
 
     fun setupBottomNav() {
-        bnvHome.setOnItemSelectedListener { item ->
+        binding.bnvHome.setOnItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.plan -> {
                     supportFragmentManager.beginTransaction().replace(R.id.fcvHome, planFragment()).commit()
--- app/src/main/java/dev/Chirii/workoutlog/signupActivity.kt
+++ app/src/main/java/dev/Chirii/workoutlog/signupActivity.kt
@@ -8,93 +8,60 @@ import android.widget.ImageView
 import android.widget.TextView
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
+import dev.Chirii.workoutlog.databinding.ActivityLoginBinding
+import dev.Chirii.workoutlog.databinding.ActivitySignupBinding
 
 class signupActivity : AppCompatActivity() {
-    lateinit var tvLogin :TextView
-    lateinit var imgFirstName: ImageView
-    lateinit var tilFirstname: TextInputLayout
-    lateinit var etFirstName: TextInputEditText
-    lateinit var imgLastName: ImageView
-    lateinit var tilLastname: TextInputLayout
-    lateinit var etLastName: TextInputEditText
-    lateinit var imgEmail: ImageView
-    lateinit var tilEmail: TextInputLayout
-    lateinit var etEmail: TextInputEditText
-    lateinit var imgPassword: ImageView
-    lateinit var etPassword: TextInputEditText
-    lateinit var tilPassword: TextInputLayout
-    lateinit var imgConfirmPassword: ImageView
-    lateinit var etConfirmPassword: TextInputEditText
-    lateinit var tilConfirmPassword: TextInputLayout
-    lateinit var btnSignup: Button
+    lateinit var binding: ActivitySignupBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivitySignupBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_signup)
+        setContentView(binding.root)
 
-        tvLogin = findViewById(R.id.tvLogin)
-        imgFirstName = findViewById(R.id.imgFirstName)
-        tilFirstname = findViewById(R.id.tilFirstname)
-        etFirstName = findViewById(R.id.etFirstName)
-        imgLastName = findViewById(R.id.imgLastName)
-        tilLastname = findViewById(R.id.tilLastname)
-        etLastName = findViewById(R.id.etLastName)
-        imgEmail = findViewById(R.id.imgEmail)
-        tilEmail = findViewById(R.id.tilEmail)
-        etEmail = findViewById(R.id.etEmail)
-        imgPassword = findViewById(R.id.imgPassword)
-        etPassword = findViewById(R.id.etPassword)
-        tilPassword = findViewById(R.id.tilPassword)
-        imgConfirmPassword = findViewById(R.id.imgConfirmPassword)
-        etConfirmPassword = findViewById(R.id.etConfirmPassword)
-        tilConfirmPassword = findViewById(R.id.tilConfirmPassword)
-        btnSignup = findViewById(R.id.btnSignup)
-
-
-
-        tvLogin.setOnClickListener {
+        binding.tvLogin.setOnClickListener {
             var intent = Intent(this,loginActivity::class.java)
             startActivity(intent)
         }
-        btnSignup.setOnClickListener {
+        binding.btnSignup.setOnClickListener {
             validateSignup()
         }
     }
     fun validateSignup(){
-        var firstname = etFirstName.text.toString()
-        var lastname = etLastName.text.toString()
-        var password = etPassword.text.toString()
-        var confirmPassword = etConfirmPassword.text.toString()
-        var email = etEmail.text.toString()
+        var firstname = binding.etFirstName.text.toString()
+        var lastname = binding.etLastName.text.toString()
+        var password = binding.etPassword.text.toString()
+        var confirmPassword = binding.etConfirmPassword.text.toString()
+        var email = binding.etEmail.text.toString()
 
         if (firstname.isBlank()){
-            tilFirstname.error = getString(R.string.first_name)
+           binding.tilFirstname.error = getString(R.string.first_name)
         }
         if (lastname.isBlank()){
-            tilLastname.error = getString(R.string.Last_name)
+            binding.tilLastname.error = getString(R.string.Last_name)
         }
         if (password.isBlank()) {
-            tilPassword.error = getString(R.string.Password)
+            binding.tilPassword.error = getString(R.string.Password)
         }
         if (password.length<8){
-            tilPassword.error = "Password is too short"
+            binding.tilPassword.error = "Password is too short"
         }
         if (password.length>16){
-            tilPassword.error = "Password is too long"
+            binding.tilPassword.error = "Password is too long"
         }
         if (email.isBlank()){
-            tilEmail.error = getString(R.string.Enter_Email)
+            binding.tilEmail.error = getString(R.string.Enter_Email)
         }
         if (confirmPassword.isBlank()){
-            tilConfirmPassword.error = getString(R.string.Confirm_password)
+            binding.tilConfirmPassword.error = getString(R.string.Confirm_password)
         }
         if (confirmPassword.length<8){
-            tilConfirmPassword.error = "Password is too short"
+            binding.tilConfirmPassword.error = "Password is too short"
         }
         if (confirmPassword.length>16){
-            tilConfirmPassword.error = "Password is too long"
+            binding.tilConfirmPassword.error = "Password is too long"
         }
-
 
     }
 
